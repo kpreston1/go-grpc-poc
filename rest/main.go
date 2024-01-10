317f4341cc164fb9534e1cbfddf9e66823cc776b
Kyle Preston <kyle.h.preston@gmail.com>
2024-01-10 09:27:37 -0600

    create todods

--- rest/main.go
+++ rest/main.go
@@ -2,8 +2,40 @@ package main
 
 import (
 	"fmt"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
 )
+
+type todo struct {
+	ID string `json:"id"`
+	Item string `json:"item"`
+	Completed bool `json:"completed"`
+}
+
+var todos = []todo {
+	{ID: "1", Item: "Buy milk", Completed: false},
+	{ID: "2", Item: "Buy eggs", Completed: false},
+	{ID: "3", Item: "Buy bread", Completed: false},
+}
+
+func getTodos(context *gin.Context) {
+	context.IndentedJSON(http.StatusOK, todos)
+}
+
+func createTodo(context *gin.Context) {
+	var newTodo todo
+	if err := context.BindJSON(&newTodo); err != nil {
+		return
+	}
+	todos = append(todos, newTodo)
+	context.IndentedJSON(http.StatusCreated, newTodo)
+}
 
 func main() {
 	fmt.Println("Hello, world!")
+	router := gin.Default()
+	router.GET("/todos", getTodos)
+	router.POST("/todos", createTodo)
+	router.Run("localhost:9090")
 }
